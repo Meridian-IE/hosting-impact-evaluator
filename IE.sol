5f8b6c57ec77d1281e0a3be04897a911a022aa76
Julian Gruber <julian@juliangruber.com>
2023-10-14 19:59:09 +0200

    evaluate on chain

--- IE.sol
+++ IE.sol
@@ -3,21 +3,47 @@
 pragma solidity ^0.8.19;
 import "./Spark.sol";
 
-// TODO: Write this as a pure on-chain IE, without external storage, since we're
-// not needing a lot of participants. Otoh, this requires participant management
-// to prevent uncontrolled growth.
-
 contract IE {
     struct Participant {
         string multiaddr;
         address payable account;
     }
+    struct Measurement {
+        Participant participant;
+        bool retrievable;
+    }
+    struct Score {
+        Participant participant;
+        uint value;
+    }
+    struct Round {
+        uint start;
+        Measurement[] measurements;
+        Participant[] participants;
+    }
 
     mapping(string => Participant) participants;
     Spark spark;
+    Round round;
+    uint roundLength = 10;
+    uint roundReward = 1 ether;
 
     constructor(Spark _spark) {
         spark = _spark;
+        advanceRound();
+    }
+
+    function advanceRound() private {
+        reward(evaluate());
+        round.start = block.number;
+        delete round.measurements;
+        delete round.participants;
+    }
+
+    function maybeAdvanceRound() private {
+        if (round.start + roundLength < block.number) {
+            advanceRound();
+        }
     }
 
     /**
@@ -51,19 +77,46 @@ contract IE {
             stringsEqual(participant.multiaddr, multiaddr),
             "Unknown participant"
         );
-        // TODO: Accumulate this data in rounds storage
-        if (retrievable) {
-            // TODO: Pay out in rounds instead
-            require(participant.account.send(0.1 ether));
+        round.measurements.push(Measurement(participant, retrievable));
+        maybeAdvanceRound();
+    }
+
+    function evaluate() private view returns (Score[] memory) {
+        Score[] memory scores = new Score[](round.participants.length);
+        for (uint i = 0; i < round.participants.length; i++) {
+            Participant memory participant = round.participants[i];
+            uint value = 0;
+            for (uint j = 0; j < round.measurements.length; j++) {
+                Measurement memory measurement = round.measurements[j];
+                if (
+                    stringsEqual(
+                        measurement.participant.multiaddr,
+                        participant.multiaddr
+                    )
+                ) {
+                    if (measurement.retrievable) {
+                        value += 1;
+                    } else {
+                        value -= 1;
+                    }
+                }
+            }
+            scores[i] = Score(participant, value);
         }
+        return scores;
     }
 
-    function evaluate() private {
-        // TODO
-    }
-
-    function reward() private {
-        // TODO
+    function reward(Score[] memory scores) private {
+      uint totalScores = 0;
+      for (uint i = 0; i < scores.length; i++) {
+        totalScores += scores[i].value;
+      }
+      for (uint i = 0; i < scores.length; i++) {
+        Participant memory participant = scores[i].participant;
+        uint value = scores[i].value;
+        uint amount = (value * roundReward) / totalScores;
+        require(participant.account.send(amount));
+      }
     }
 
     function stringsEqual(
